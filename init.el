d3bd5e6c59288cff820150b29bb3d204bbc41f65
amalson <august.malson@journalexperts.com>
2014-11-13 17:25:06 -0500

    [merging features]

--- init.el
+++ init.el
@@ -203,9 +203,4 @@
            (set-face-background 'mode-line (car color))
            (set-face-foreground 'mode-line (cdr color))))))
 
-;;; Sadly this does not appear to work right now ...
-(load "elscreen" "ElScreen" t)
-    (define-key evil-normal-state-map (kbd "C-w t") 'elscreen-create) ;creat tab
-    (define-key evil-normal-state-map (kbd "C-w x") 'elscreen-kill) ;kill tab
-    (define-key evil-normal-state-map "gT" 'elscreen-previous) ;previous tab
-    (define-key evil-normal-state-map "gt" 'elscreen-next) ;next tab
+;;; init.el ends here
